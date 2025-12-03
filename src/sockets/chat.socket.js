b66fa57b922634d5e3abf4358cf19150f3a41959
Sontara444 <sontararajput@gmail.com>
2025-12-03 23:07:25 +0530

    feat: implement Socket.IO server for real-time chat, user authentication, and message handling.

--- src/sockets/chat.socket.js
+++ src/sockets/chat.socket.js
@@ -8,7 +8,7 @@ let io;
 function initSocket(server) {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: process.env.CLIENT_URL,
       methods: ["GET", "POST"]
     }
   });
